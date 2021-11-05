2dfd1d1c2865a0bb69a6efe4f182b499023ffcc7
nasiegel88 <nasiegel@ucdavis.edu>
2021-11-05 08:54:20 -0700

    populate

---
+++ R/download-data.r
@@ -0,0 +1,16 @@
+## Download data from GEO database
+
+temp.control_LD09 <- paste(tempfile(),".h5",sep = "")
+download.file("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSM4511886&format=file&file=GSM4511886%5Fcontrol%5FLD09%5Ffiltered%5Ffeature%5Fbc%5Fmatrix%2Eh5", 
+              temp.control_LD09, mode = "wb")
+control_31810_h5 <- Read10X_h5(control_LD09)
+
+temp.control_31810 <- paste(tempfile(),".h5",sep = "")
+download.file("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSM4511885&format=file&file=GSM4511885%5Fcontrol%5F31810%5Ffiltered%5Ffeature%5Fbc%5Fmatrix%2Eh5", 
+              temp.control_31810, mode = "wb")
+control_31810_h <- Read10X_h5(control_31810)
+
+temp.control_LE99 <- paste(tempfile(),".h5",sep = "")
+download.file("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSM4511887&format=file&file=GSM4511887%5Fcontrol%5FLE99%5Ffiltered%5Ffeature%5Fbc%5Fmatrix%2Eh5", 
+              temp.control_LE99, mode = "wb")
+control_LE99_h5 <- Read10X_h5(control_LE99)
---
+++ R/packages.r
@@ -0,0 +1,130 @@
+if(!require(renv)){
+  install.packages("renv")
+  library('renv')
+}
+# bioconductor
+if(!require(BiocManager)){
+  install.packages("BiocManager")
+  library('BiocManager')
+}
+if(!require(rmarkdown)){
+  install.packages("rmarkdown", dep = TRUE)
+  library('rmarkdown')
+}
+if(!require(tinytex)){
+  install.packages("tinytex")
+  tinytex::install_tinytex()
+}
+if(!require(devtools)){
+  install.packages("devtools")
+  library('devtools')
+}
+if(!require(remotes)){
+  install.packages('remotes')
+  library('remotes')
+}
+if(!require(reticulate)){
+  install.packages("reticulate")
+  Sys.setenv(RETICULATE_PYTHON=miniconda_path())
+  library('reticulate')
+}
+if(!require(sctransform)){
+  devtools::install_github(repo = 'ChristophH/sctransform')
+  library(sctransform)
+}
+if(!require(Seurat)){
+  devtools::install_github(repo = "satijalab/seurat", ref = "develop")
+  library(Seurat)
+}
+if(!require(scCATCH)){
+  devtools::install_github('ZJUFanLab/scCATCH')
+  library('scCATCH')
+}
+if(!require(patchwork)){
+  install.packages("patchwork")
+  library('patchwork')
+}
+if(!require(dplyr)){
+  install.packages("dplyr")
+  library('dplyr')
+}
+if(!require(ggplot2)){
+  install.packages("ggplot2")
+  library('ggplot2')
+}
+if(!require(kableExtra)){
+  install.packages("kableExtra")
+  library('kableExtra')
+}
+if(!require(purrr)){
+  install.packages("purrr")
+  library('purrr')
+}
+if(!require(gdtools)){
+  install.packages("gdtools")
+  library('gdtools')
+}
+if(!require(flextable)){
+  install.packages("flextable")
+  library('flextable')
+}
+if(!require(pracma)){
+  install.packages("pracma")
+  library('pracma')
+}
+if(!require(stringr)){
+  install.packages("stringr")
+  library('stringr')
+}
+if(!require(data.table)){
+  install.packages("data.table")
+  library('data.table')
+}
+# RNA velocity using python
+if(!require(SeuratDisk)){
+  remotes::install_github("mojaveazure/seurat-disk")
+  library(SeuratDisk)
+}
+if(!require(SeuratWrappers)){
+  remotes::install_github('satijalab/seurat-wrappers')
+  library(SeuratWrappers)
+}
+if(!require(velocyto.R)){
+  install_github("velocyto-team/velocyto.R")
+  library(velocyto.R)
+}
+# Bioconductor
+if(!require(multtest)){
+  BiocManager::install("multtest")
+  library('multtest')
+}
+if(!require(metap)){
+  BiocManager::install("metap")
+  library('metap')
+}
+if(!require(escape)){
+  BiocManager::install("escape")
+  library('escape')
+}
+if(!require(dittoSeq)){
+  BiocManager::install("dittoSeq")
+  library('dittoSeq')
+}
+if(!require(SingleCellExperiment)){
+  BiocManager::install("SingleCellExperiment")
+  library('SingleCellExperiment')
+}
+if(!require(pcaMethods)){
+  BiocManager::install("pcaMethods")
+  library(pcaMethods)
+}
+if(!require(EnhancedVolcano)){
+  BiocManager::install("EnhancedVolcano")
+  library('EnhancedVolcano')
+}
+
+
+if(!require(rhdf5)){
+  BiocManager::install("rhdf5")
+  library('rhdf5')
+}
