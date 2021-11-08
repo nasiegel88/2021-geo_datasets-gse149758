98855858e101f17e16d86461e17bc89422fe82eb
nasiegel88 <nasiegel@ucdavis.edu>
2021-11-08 10:19:53 -0800

    fix download link

--- R/download-data.r
+++ R/download-data.r
@@ -3,7 +3,7 @@
 temp.control_LD09 <- paste(tempfile(),".h5",sep = "")
 download.file("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSM4511886&format=file&file=GSM4511886%5Fcontrol%5FLD09%5Ffiltered%5Ffeature%5Fbc%5Fmatrix%2Eh5", 
               temp.control_LD09, mode = "wb")
-control_31810_h5 <- Read10X_h5(temp.control_LD09)
+control_LD09_h5 <- Read10X_h5(temp.control_LD09)
 
 temp.control_31810 <- paste(tempfile(),".h5",sep = "")
 download.file("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSM4511885&format=file&file=GSM4511885%5Fcontrol%5F31810%5Ffiltered%5Ffeature%5Fbc%5Fmatrix%2Eh5", 
