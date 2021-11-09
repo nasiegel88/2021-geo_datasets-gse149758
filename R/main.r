7e3aac5c2a9ae6fce96d40324f086d5746484c8a
nasiegel88 <nasiegel@ucdavis.edu>
2021-11-09 14:02:58 -0800

    update figs

--- R/main.r
+++ R/main.r
@@ -1,6 +1,7 @@
 source("R/packages.r")
+source("R/dir.r")
 
-if (length(ls(pattern = "_h5")) == 0) {source("R/download-data.r")}
+#if (length(ls(pattern = "_h5")) == 0) {source("R/download-data.r")}
 
 ## Using Azimuth for cluster annotation instead of scCATCH
 # if (length(list.files(path = "./output/rdata")) == 0) {source("R/combine-objects.r")} 
